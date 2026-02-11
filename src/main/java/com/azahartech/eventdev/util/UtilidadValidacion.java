234961ec84b4efe3b395f5dab8430441e92a209a
Junfeng Wangli <junwan@alu.edu.gva.es>
2026-02-11 16:24:15 +0100

    Feat: Creacion del metodo generarIdAutomatico en la clase UtilidadValidacion

--- src/main/java/com/azahartech/eventdev/util/UtilidadValidacion.java
+++ src/main/java/com/azahartech/eventdev/util/UtilidadValidacion.java
@@ -1,5 +1,7 @@
 package com.azahartech.eventdev.util;
 
+import java.time.LocalDate;
+
 public class UtilidadValidacion {
     /**
      *
@@ -9,5 +11,49 @@ public class UtilidadValidacion {
      */
     public static boolean esEmailValido(String email) {
         return email.matches("[a-zA-Z0-9\\.]+@[a-zA-Z0-9\\.]+\\.[a-z]{2,4}");
+    }
+
+    /**
+     * @param fecha
+     * Comprueba que la fecha de un evento sea futura.
+     * */
+    public static boolean esFechaFutura(LocalDate fecha) {
+        if (fecha == null) {
+            return false;
+        } else {
+            return fecha.isAfter(LocalDate.now());
+        }
+    }
+    /**
+     * Valida si un telefono tiene 9 digitos
+     * @param telefono
+     * @return
+     */
+    public static boolean esTelefonoValido(String telefono) {
+        return telefono.matches("^[0-9]{9}");
+    }
+
+    /**
+     * Este metodo genera un ID Automaticamente
+     *
+     * Formato:
+     * 1 Palabra: Los 5 primeros caracteres
+     * 5 Palabra: Cada sigla de una palabra
+     *
+     * @param nombre
+     * @return devuelve un ID
+     */
+    public static String generaraIdAutomatico(String nombre){
+        final String PREDETERMINADO = "DEFAU";
+        nombre = (nombre != null) ? nombre : PREDETERMINADO;
+        String resultado;
+        int año = LocalDate.now().getYear();
+
+        String siglas = nombre.toUpperCase().substring(0, (nombre.length() > 5)? 5: nombre.length());
+
+
+        resultado = String.format("EVT-%s-%s", año, siglas);
+
+        return resultado;
     }
 }
