897fc3a7c66ac4c814de655ddb96573c757f9074
Jonathan Rosa <azukijra@gmail.com>
2026-02-20 20:55:48 +0100

    Feat: Creacion clase Tarjeta Evento (a medias)

---
+++ src/main/java/com/azahartech/eventdev/vista/TarjetaEvento.java
@@ -0,0 +1,16 @@
+package com.azahartech.eventdev.vista;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TarjetaEvento extends JPanel {
+
+    public TarjetaEvento(String titulo, String fecha, String precio){
+        JPanel tarjetaEvento = new JPanel(new BorderLayout(10, 10));
+        BorderFactory.createLineBorder(Color.GRAY);
+
+
+
+    }
+
+}
